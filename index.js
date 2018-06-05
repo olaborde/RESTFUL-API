09e71b7c6e0bf59a83cf3a91ca1779d8353cf25f
olaborde <oml007@live.com>
2018-06-04 21:40:31 -0400

    added routing endpoints

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import express from 'express';
+const express      = require('express');  
+import routes from './src/routes/crmRoutes'
 
 const app = express();
 const PORT =3000;
 
-
+routes(app);
 app.get('/', (req, res) =>
   res.send(`Node and express server is running on port ${PORT} `)
 );
 
+
 app.listen(PORT, () =>
   console.log(`you server is running on port ${PORT}`)
 );
+
+module.exports = app;
