00182832f8801c464f6eaddf6156a3bcf0197299
olaborde <oml007@live.com>
2018-06-05 00:47:51 -0400

    created schema

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express      = require('express');  
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes'
 
 const app = express();
 const PORT =3000;
+
+// mongoose connection
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb');
+
+//
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 app.get('/', (req, res) =>
---
+++ src/models/crmModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema ({
+    firstName: {
+      type: String,
+      required: 'Enter a first name'
+    },
+    lastName: {
+      type: String,
+      required: 'Enter a last name'
+    },
+    email: {
+      type: String,
+    
+    },
+    email: {
+      company: String,
+    
+    },
+    phonw: {
+      type: Number,
+    
+    },
+    created_date: {
+
+      type: Date,
+      default: Date.now
+
+    }
+
+})
