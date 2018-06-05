fdc2584b7c32d83dcf5c676787499307cccb6738
olaborde <oml007@live.com>
2018-06-04 23:52:39 -0400

    middlewared added

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,14 @@
 const routes = (app) => {
   app.route('/contact')
-  .get((req, res) =>
-  res.send('GET request successfull!!!'))
+  .get((req, res, next) => { 
+    // moddleware
+    console.log( `Request from ${req.originalUrl}`)
+    console.log( `Request from ${req.method}`)
+    next();
+  }, (req, res, next) => {
+    
+
+  })
 
   .post((req, res) =>
   res.send('POST request successfull!!!'));
